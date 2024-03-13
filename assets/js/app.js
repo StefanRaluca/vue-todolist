0a416196d2d9330179e59ab5602e53b55b8f0566
StefanRaluca <raluca.bubulina@yahoo.com>
2024-03-13 15:09:05 +0100

    add milestone 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,17 +4,27 @@ createApp({
     data() {
         return {
             todos: [
-                { text: 'Pagare la palestra', done: false },
+                { text: 'Pagare la palestra', done: true },
                 { text: 'Pagare la bolleta della luce', done: true },
                 { text: 'Studiare per la verifica', done: false },
                 { text: 'Andare dal dottore', done: false },
                 { text: 'Prendere le medicine dalla farmacia', done: true },
-            ]
+            ],
         }
+
+    },
+    methods: {
+        canceltodo(i) {
+            //cancella il todos che viene clickato
+            this.todos.splice(i, 1);
+        },
+
+
+
     }
 })
     .mount('#app')
 
 
 
-    console.log('it work');
+console.log('it work');
