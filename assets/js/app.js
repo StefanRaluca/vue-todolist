2ab45cc092fde92b73d1ad7d9b4423383826a8fa
StefanRaluca <raluca.bubulina@yahoo.com>
2024-03-13 15:15:05 +0100

    add bonus 1 keypress ad input

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,6 +10,9 @@ createApp({
                 { text: 'Andare dal dottore', done: false },
                 { text: 'Prendere le medicine dalla farmacia', done: true },
             ],
+
+            // facciamo una stringa vuota per aggiungere il nuovo todo
+            newTodo: '',
         }
 
     },
@@ -17,6 +20,20 @@ createApp({
         canceltodo(i) {
             //cancella il todos che viene clickato
             this.todos.splice(i, 1);
+        },
+
+        addTodo() {
+            //console.log(this.addTodo);
+
+            //facciamo una condizione per non stampare in pagina i spazi vuoti 
+            if (this.newTodo.trim() !== ('')) {
+
+                this.todos.push({ text: this.newTodo, done: false });
+
+                // Svuota il campo di input dopo l aggiunta del todo
+                this.newTodo = '';
+
+            }
         },
 
 
